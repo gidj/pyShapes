abeb5f44e2bd7176eb1cb1a6f39bf33d015616f4
Gideon VanRiette <gvanriette@gmail.com>
2014-05-04 17:03:23 -0500

    Added skeleton classes for test cases

---
+++ tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+class LinesTestCases(unittest.TestCase):
+    pass
+
+class CirclesTestCases(unittest.TestCase):
+    pass
+
+class PolygonTestCases(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
