6bfe7d14306c5c44eaeff9b0619f69dda48ab31c
Gideon VanRiette <gvanriette@gmail.com>
2014-05-04 17:15:37 -0500

    adding unit tests to test.py

--- tests.py
+++ tests.py
@@ -1,13 +1,24 @@
 import unittest
+import shapes
 
 class LinesTestCases(unittest.TestCase):
-    pass
+    """Test cases with only lines"""
+    def setUp(self):
+        pt1 = Point(0, 0)
+        pt2 = point(1, 1)
+        pass
+    
 
 class CirclesTestCases(unittest.TestCase):
-    pass
+    """Test cases with only circles"""
+    def setUp(self):
+        pass
+
 
 class PolygonTestCases(unittest.TestCase):
-    pass
+    """Test cases with only polygons"""
+    def setUp(self):
+        pass
 
 
 if __name__ == '__main__':
